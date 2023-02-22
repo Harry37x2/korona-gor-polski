870b36f388ffdb140c7ecaaf9a268a746807ce58
Harry37x2 <bartek3737@o2.pl>
2023-02-22 22:44:22 +0100

    feat: set date and time and patch in json

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,39 +1,76 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import Peak from './Peak'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Peak from "./Peak";
+import moment from "moment";
 
-const url = 'http://localhost:3000/peaks'
+const url = "http://localhost:3000/peaks";
 const Dashboard = () => {
-    const [peaksList, setPeaksList] = useState([])
-    const fetchData = async () => {
-        try {
-            const {data} = await axios(url,{
-                headers: {
-                    Accept: 'application/json'
-                }
-            })
-            setPeaksList(data)
-        } catch (error) {
-            console.log(error.response)
-        }
-    }
-    
-    useEffect(()=>{
-        fetchData();
-        console.log(peaksList)
-    },[])
-    
-    
-    return (
-    <>
-    {peaksList.map((peak)=>(
-        <Peak
-            key={peak.id}
-            peak={peak}
-        />
-    ))}
-    </>
-  )
-}
+  const [peaksList, setPeaksList] = useState([]);
+  const [expanded, setExpanded] = useState(false);
+  const [date, setDate] = useState(moment());
+  const currentDate = new Date().toLocaleDateString();
+  console.log(currentDate);
 
-export default Dashboard
+  const fetchData = async () => {
+    try {
+      const { data } = await axios(url, {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      setPeaksList(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  const handleSubmit = async (id) => {
+    console.log(id);
+    const submitedList = peaksList.map((item) =>
+      item.id === id
+        ? { ...item, visited: true, date: date.format("D/MM/YYYY, H:mm") }
+        : item
+    );
+    setPeaksList(submitedList);
+    const elementUrl = `${url}/${id}`;
+    const elementToUpdate = submitedList.filter((item) => item.id === id);
+    const valueToUpdate = {
+      visited: elementToUpdate[0].visited,
+      date: elementToUpdate[0].date,
+    };
+    console.log(valueToUpdate);
+    console.log(date);
+
+    const response = await axios
+      .patch(elementUrl, valueToUpdate)
+      .catch((error) => console.log(error));
+    // console.log(toUpdate[0].visited);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function dateChangeHandler(value) {
+    setDate(value);
+    console.log(date.format("D/MM/YYYY, h:mm a"));
+  }
+
+  return (
+    <>
+      {peaksList.map((peak) => (
+        <Peak
+          date={date}
+          dateChangeHandler={dateChangeHandler}
+          key={peak.id}
+          peak={peak}
+          handleSubmit={handleSubmit}
+          expanded={expanded}
+          setExpanded={setExpanded}
+        />
+      ))}
+    </>
+  );
+};
+
+export default Dashboard;
--- src/Peak.jsx
+++ src/Peak.jsx
@@ -1,58 +1,116 @@
-import React from 'react'
-import { useState } from 'react';
-import { Accordion, AccordionSummary, AccordionDetails,Typography, Button } from '@mui/material'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
-import HeightIcon from '@mui/icons-material/Height';
-import TimelapseIcon from '@mui/icons-material/Timelapse';
-import TrendingUpIcon from '@mui/icons-material/TrendingUp';
-import ModeOfTravelIcon from '@mui/icons-material/ModeOfTravel';
-import WhereToVoteIcon from '@mui/icons-material/WhereToVote';
+import React from "react";
+import { useState } from "react";
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+  Button,
+} from "@mui/material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import HeightIcon from "@mui/icons-material/Height";
+import TimelapseIcon from "@mui/icons-material/Timelapse";
+import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import ModeOfTravelIcon from "@mui/icons-material/ModeOfTravel";
+import WhereToVoteIcon from "@mui/icons-material/WhereToVote";
+import DateAndTimePicker from "./DateAndTimePicker";
+import moment from "moment";
 
-
-const Peak = ({peak}) => {
-    const [expanded, setExpanded] = useState(false)
-    const handleChange = (isExpanded, panel) => {
-        setExpanded(isExpanded ? panel : false)
-    }
-    
+const Peak = ({
+  peak,
+  handleSubmit,
+  expanded,
+  setExpanded,
+  date,
+  dateChangeHandler,
+}) => {
+  const handleChange = (isExpanded, panel) => {
+    setExpanded(isExpanded ? panel : false);
+  };
 
   return (
     <Accordion
-        expanded={expanded === `panel${peak.id}`}
-        onChange={(event, isExpanded) => handleChange(isExpanded, `panel${peak.id}`)}
+      expanded={expanded === `panel${peak.id}`}
+      onChange={(event, isExpanded) =>
+        handleChange(isExpanded, `panel${peak.id}`)
+      }
     >
-        <AccordionSummary 
-            id={`panel-${peak.id}-header`}
-            aria-controls={`panel-${peak.id}-content`}
-            expandIcon={<ExpandMoreIcon/>}
-            variant={`${peak.visited === true ? 'done' : 'default'}`}
+      <AccordionSummary
+        id={`panel-${peak.id}-header`}
+        aria-controls={`panel-${peak.id}-content`}
+        expandIcon={<ExpandMoreIcon />}
+        variant={`${peak.visited === true ? "done" : "default"}`}
+      >
+        <Typography
+          sx={{
+            width: "35%",
+            flexShrink: 0,
+            display: "flex",
+            justifyContent: "flex-start",
+          }}
         >
-            <Typography sx={{ width: '35%', flexShrink: 0, display:'flex', justifyContent: 'flex-start'}}>
-                {`${peak.name}`}<HeightIcon/>{`${peak.altitude}m n.p.m`}
-            </Typography>
-            <Typography>                
-                {peak.visited===true ? <span style={{display: 'flex'}}><WhereToVoteIcon/>{`Zdobyto dnia: ${peak.date}`}</span> : '' }
-            </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-            <div className='peakDetails'>
-                <div className='peakDetails-left'>
-                    <p>{`${peak.chain}`}</p>
-                    <p>{`${peak.name}`}</p>
-                    <HeightIcon/><span>{`${peak.altitude}m n.p.m`}</span>
-                    <TrendingUpIcon/><span>{`${peak.elevation}m`}</span>
-                    <ModeOfTravelIcon/><span>{`${peak.distance}km`}</span>
-                    <TimelapseIcon/><span>{`${peak.time}`}</span>
-                </div>
-                <div className='peakDetails-right'>
-                    {peak.visited === false 
-                    ? <Button variant='contained'>Zatwierdź</Button>
-                    : <Button variant='contained' disabled>Zdobyto</Button>}
-                </div>
-            </div>
-        </AccordionDetails>
+          {`${peak.name}`}
+          <HeightIcon />
+          {`${peak.altitude}m n.p.m`}
+        </Typography>
+        <Typography>
+          {peak.visited === true ? (
+            <span style={{ display: "flex" }}>
+              <WhereToVoteIcon />
+              {`Zdobyto dnia: ${peak.date}`}
+            </span>
+          ) : (
+            ""
+          )}
+        </Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        <div className="peakDetails">
+          <div className="peakDetails-left">
+            <p>{`${peak.chain}`}</p>
+            <p>{`${peak.name}`}</p>
+            <HeightIcon />
+            <span className="tooltip">
+              {`${peak.altitude}m n.p.m`}
+              <span className="tooltiptext">{"wys. bezwzględna"}</span>
+            </span>
+            <TrendingUpIcon />
+            <span className="tooltip">
+              {`${peak.elevation}m`}
+              <span className="tooltiptext">{"suma podejść"}</span>
+            </span>
+            <ModeOfTravelIcon />
+            <span className="tooltip">
+              {`${peak.distance}km`}
+              <span className="tooltiptext">{"dystans"}</span>
+            </span>
+            <TimelapseIcon />
+            <span className="tooltip">
+              {`${peak.time}`}
+              <span className="tooltiptext">{"czas"}</span>
+            </span>
+          </div>
+          <div className="peakDetails-right">
+            <DateAndTimePicker onChange={dateChangeHandler} date={date} />
+            {peak.visited === false ? (
+              <Button
+                variant="contained"
+                onClick={() => {
+                  handleSubmit(peak.id);
+                }}
+              >
+                Zatwierdź
+              </Button>
+            ) : (
+              <Button variant="contained" disabled>
+                Zdobyto
+              </Button>
+            )}
+          </div>
+        </div>
+      </AccordionDetails>
     </Accordion>
-  )
-}
+  );
+};
 
-export default Peak
+export default Peak;
